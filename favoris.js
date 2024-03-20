26de0fe070f761c3a695e715b4f7cd3677c177a7
VUILLET Gaspard <vuilletg@hostname.iut2.upmf-grenoble.fr>
2024-03-20 16:17:18 +0100

    favorit recup données

--- favoris.js
+++ favoris.js
@@ -1,2 +1,17 @@
-localStorage.favoris = new Array();
-localStorage.favoris[] = "hello world"
+favoris = localStorage.favoris.split("|");
+if
+ajoutBtn = document.getElementById("btn-favoris");
+champ = document.querySelector("input[type = 'text']");
+favorisList = document.getElementById("liste-favoris");
+favorisVide = document.getElementById("favoris-vide");
+start();
+function start() {
+    if (favoris.lenght <= 1) {
+        favorisVide.style.visibility = "show";
+    }else{
+        favorisVide.style.visibility = "hidden";
+        for (let i = 0; i < favoris.length; i++) {
+
+        }
+    }
+}
